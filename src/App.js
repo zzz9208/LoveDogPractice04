2d0c735853403450af6e25c18b6f19a8fc31e730
JiHyeon <zzz9208@naver.com>
2024-02-02 05:36:28 +0900

    KEY_TEST01

--- src/App.js
+++ src/App.js
@@ -2,21 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const myText = process.env.REACT_APP_ACTION_TEXT;
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload.sdfasdf
+          {myText}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    
       </header>
     </div>
   );
