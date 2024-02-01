5d9f67d28db88e6115f2447a2882d6230c9f2dbd
JiHyeon <zzz9208@naver.com>
2024-02-02 05:50:56 +0900

    KEY_TEST_06

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 
 function App() {
   const myText = process.env.REACT_APP_ACTION_TEXT;
+  console.log("my text is,,,,,,,,,,,")
+  console.log(myText)
   return (
     <div className="App">
       <header className="App-header">
         <p>
-          Edit 05
+          Edit 06
           {myText}
         </p>
     
