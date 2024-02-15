7503450bdd938fd0fd5dcc09837894e77a792f32
JiHyeon <zzz9208@naver.com>
2024-02-16 04:18:40 +0900

    mobile check01

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
+import { useEffect } from 'react';
 import './App.css';
 
 // secret 키 바꾸고, 깃헙에 재업로드해야 반영
 
 function App() {
+  useEffect(() => {
+    const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+    if (isMobile) {
+      // mobile
+      //setMobile(true);
+      console.log("Mobile")
+    } else {
+      // desktop
+      //setMobile(false);
+      console.log("Not Mobile")
+    }
+  }, []);
+
+  
   const myText = process.env.REACT_APP_ACTION_TEXT;
   console.log("my text is,,,,,,,,,,,")
   console.log(myText)
