e17012ad90312bf9ff65dc7bb7116daf093f6a20
JiHyeon <zzz9208@naver.com>
2024-02-16 11:08:52 +0900

    router 12

--- src/Main.js
+++ src/Main.js
@@ -8,34 +8,9 @@ const Main = (props) => {
         movePage('/core');
     }
     function exithome(){
-        //movePage("https://google.com");
         window.close();
-        // if ( $.browser.msie ) {	
-        //     window.opener='Self';	
-        //     window.open('','_parent','');	
-        //     window.close();
-        // } else {	
-        //     window.close(); // 일반적인 현재 창 닫기	
-        //     window.open('about:blank','_self').self.close();  // IE에서 묻지 않고 창 닫기
-        // }
     }
 
-
-
-    //const [mobile, setMobile] = useState(null);
-    // useEffect(() => {
-    //   const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
-    //   if (isMobile) {
-    //     // mobile   
-    //     setMobile(true);
-    //     console.log("Mobile", mobile, mobile !== null ? mobile.toString(): "null" )
-    //   } else {
-    //     window.close();
-    //     // desktop      
-    //     setMobile(false);
-    //     console.log("Not Mobile", mobile, mobile !== null ? mobile.toString(): "null")
-    //   }
-    // }, [mobile]);
 
 
     useEffect(() => {
@@ -47,16 +22,13 @@ const Main = (props) => {
         // console.log("Mobile", mobile, mobile !== null ? mobile.toString(): "null" )
          } else {
             console.log("Mobile")
-            function WinClose(){
-                window.open('http://www.naver.com?','_self').close();
-                //window.opener=self;
-                //window.close();
-            }
-            setTimeout(WinClose(), 3000)
-            // window.close();
-          // desktop      
-        //   setMobile(false);
-        //   console.log("Not Mobile", mobile, mobile !== null ? mobile.toString(): "null")
+            window.open('http://www.naver.com?','_self').close();
+            // function WinClose(){
+            //     window.open('http://www.naver.com?','_self').close();
+            //     //window.opener=self;
+            //     //window.close();
+            // }
+            // setTimeout(WinClose(), 3000)
         }
       }, []);
   
@@ -67,7 +39,7 @@ const Main = (props) => {
         <button onClick={() => window.open("https://google.com")}>google</button>
         <button onClick={() => gohome()}>core</button>
         <button onClick={() => exithome()}>외부</button>
-		<h3>안녕하세요. 메인페이지11 입니다.</h3>
+		<h3>안녕하세요. 메인페이지12 입니다.</h3>
         <p>
           my device is 
           {/* {ismobile !== null ? ismobile.toString(): "null"} */}
