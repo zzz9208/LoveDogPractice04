7e347a8a147da3c270f98dc693dac2bcbdf9cd1e
JiHyeon <zzz9208@naver.com>
2024-02-16 10:45:47 +0900

    router 08

--- src/Main.js
+++ src/Main.js
@@ -46,7 +46,7 @@ const Main = (props) => {
         //   console.log("Mobile", mobile, mobile !== null ? mobile.toString(): "null" )
         // console.log("Mobile", mobile, mobile !== null ? mobile.toString(): "null" )
          } else {
-            onsole.log("Mobile")
+            console.log("Mobile")
             window.open("", "_blank", "");
             window.close();
           // desktop      
@@ -62,7 +62,7 @@ const Main = (props) => {
         <button onClick={() => window.open("https://google.com")}>google</button>
         <button onClick={() => gohome()}>core</button>
         <button onClick={() => exithome()}>외부</button>
-		<h3>안녕하세요. 메인페이지07 입니다.</h3>
+		<h3>안녕하세요. 메인페이지08 입니다.</h3>
         <p>
           my device is 
           {/* {ismobile !== null ? ismobile.toString(): "null"} */}
