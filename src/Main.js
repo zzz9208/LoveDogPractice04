1e4c4be4a70c144ff6bdf967c8d2d3c44c686541
JiHyeon <zzz9208@naver.com>
2024-02-16 09:31:50 +0900

    router 03

--- src/Main.js
+++ src/Main.js
@@ -12,12 +12,37 @@ const Main = (props) => {
     }
 
 
+
+
+    const [mobile, setMobile] = useState(null);
+
+    useEffect(() => {
+      const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+      if (isMobile) {
+        // mobile   
+        setMobile(true);
+        console.log("Mobile", mobile, mobile !== null ? mobile.toString(): "null" )
+      } else {
+        // desktop      
+        setMobile(false);
+        console.log("Not Mobile", mobile, mobile !== null ? mobile.toString(): "null")
+      }
+    }, [mobile]);
+
+
+
+
+
 	return (
 		<>
         <button onClick={() => window.open("https://google.com")}>google</button>
         <button onClick={() => gohome()}>core</button>
         <button onClick={() => exithome()}>외부</button>
-		<h3>안녕하세요. 메인페이지01 입니다.</h3>
+		<h3>안녕하세요. 메인페이지03 입니다.</h3>
+        <p>
+          my device is 
+          {mobile !== null ? mobile.toString(): "null"}
+        </p>
 		</>
 	);
 };
